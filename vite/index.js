02d5b74cfecab2aa51e6f410afee1201305e6bd4
ericjin <eric4336355@gmail.com>
2021-10-29 09:37:03 +0800

    feat: 新增js文件请求

--- vite/index.js
+++ vite/index.js
@@ -2,7 +2,7 @@
  * @Author jinxiaodong
  * @Date 2021-10-26 09:35:02
  * @LastEditors jinxiaodong
- * @LastEditTime 2021-10-28 09:32:37
+ * @LastEditTime 2021-10-29 09:35:48
  * @Desc web server
  */
 const Koa = require('koa')
@@ -15,11 +15,16 @@ app.use(async (ctx) => {
   console.log('url:', url)
   if (url === '/') {
     ctx.type = 'text/html'
-    const content = fs.readFileSync('./index.html', 'utf-8')
+    const content = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8')
+    ctx.body = content
+  } else if (url.endsWith('js')) {
+    const jsPath = path.resolve(__dirname, url.slice(1))
+    ctx.type = 'application/javascript'
+    const content = fs.readFileSync(jsPath, 'utf-8')
     ctx.body = content
   }
 })
 
 app.listen(3000, () => {
-  console.log('server ready')
+  console.log('server ready: http://localhost:3000')
 })
